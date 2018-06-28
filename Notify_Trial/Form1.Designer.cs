5977796d127168484706e8cf4a8efe9f069f47ee
RanjanChoudhury <Pradeepta_Ranjan_cho@Dell.com>
2018-06-28 16:50:37 +0530

    first commit

---
+++ Notify_Trial/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Notify_Trial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(242, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "event name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Notify_Trial/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+using System.IO;
+using System.Data.SqlClient;
+namespace Notify_Trial
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*PopupNotifier popup = new PopupNotifier();
+            popup.Image = Properties.Resources.if_info_370079;
+            popup.ImageSize = new Size(30, 30);
+            popup.TitleText = "first notification";
+            popup.ContentText = "this is my first notification";
+            popup.Popup();*/
+           /* notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(1000, "Important Notice", "Something has come up", ToolTipIcon.Info);
+    */        
+            /*FileStream fs = new FileStream("c:\\Users\\Pradeepta_Ranjan_Cho\\Desktop\\files\\events.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs); */
+            string event1 = textBox1.Text;
+            /*sw.WriteLine(event1);
+            sw.Flush();
+            sw.Close();
+            fs.Close();*/
+            label2.Text = "your event has been registered";
+            textBox1.Text = "";
+
+            SqlConnection sqlConnection1 =
+              new SqlConnection(@"Data Source=mssql5.gear.host;Initial Catalog=events3;User ID=events3;Password=Ev067I!~zyZy");
+               
+            DateTime today = DateTime.Now;
+
+            string query = "INSERT Events2 (EventName,Upload_Time) VALUES ('"+event1+"','"+today+"')";
+            sqlConnection1.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO EVENTS2 (EventName,Upload_Time) VALUES (@Name,@Time) ",sqlConnection1);
+            cmd.Parameters.AddWithValue("@Name", event1);
+            cmd.Parameters.AddWithValue("@Time", today);
+            cmd.ExecuteNonQuery();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
